19a97fa985c1c70711a2e776e80dfe2ad69a2127
Anthony <faderkay93@gmail.com>
2023-04-15 13:30:28 -0700

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,7 @@
  *
  * Return: 0 (Success), 1 (Error)
  */
-int main(int argc, cgar *argv[])
+int main(int argc, char *argv[])
 {
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
@@ -33,7 +33,7 @@ int main(int argc, cgar *argv[])
 	{
 		while (num >= coins[j])
 		{
-			results++;
+			result++;
 			num -= coins[j];
 		}
 	}
