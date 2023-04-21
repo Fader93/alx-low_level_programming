f98d906f2f35c64f48683592ad47be4874db35b6
Anthony <faderkay93@gmail.com>
2023-04-21 08:14:06 -0700

    Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,16 @@
+#Include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str, *sep = "";
+
+	va_list list;
+
+	va_start(list, format);
